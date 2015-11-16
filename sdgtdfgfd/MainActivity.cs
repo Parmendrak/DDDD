cf76ac545598ec5b9eaf08d829588f1610fcd7cf
Parmendrak <parmendrak@360logica.com>
2015-11-16 22:39:22 +0530

    commit3

--- sdgtdfgfd/MainActivity.cs
+++ sdgtdfgfd/MainActivity.cs
@@ -20,7 +20,7 @@ namespace sdgtdfgfd
 			// Get our button from the layout resource,
 			// and attach an event to it
 			Button button = FindViewById<Button>(Resource.Id.myButton);
-			Console.WriteLine ("Xamarin");
+			Console.WriteLine ("P");
 			button.Click += delegate {
 				button.Text = string.Format("{0} clicks!", count++);
 			};
